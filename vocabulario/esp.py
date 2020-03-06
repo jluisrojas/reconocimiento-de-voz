72e1458f3007ac1ad2ee4f8bf548144f63fc78ce
Jose Luis <joseluisrojasaranda@gmail.com>
2020-03-05 18:05:25 -0600

    Creacion de vocabulario en espaniol

---
+++ vocabulario/esp.py
@@ -0,0 +1,39 @@
+import tensorflow as tf
+
+class EspVocabulario:
+    def __init__(self, incluir_simbolos=False):
+        self.caracteres = [
+            "a", "b",
+            "c", "d",
+            "e", "f",
+            "g", "h",
+            "i", "j",
+            "k", "l",
+            "m", "n",
+            "o", "p",
+            "q", "r",
+            "s", "t",
+            "u", "v",
+            "w", "x",
+            "y", "z",
+            " "
+        ]
+
+        self.vocab = {}
+        for i, c in enumerate(self.caracteres):
+            self.vocab[c] = i
+
+    def __call__(self, cadena):
+        res = []
+        cadena = cadena.lower()
+        for c in cadena:
+            if c in self.vocab:
+                res.append(self.vocab[c])
+
+        return res
+
+    def get_config():
+        config = {
+            "vocabulario": self.vocab
+        }
+        return config
